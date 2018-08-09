876fbdc52c612c0fc2e6683d550b8b9d3ec627b5
Kevin Chi <kevinchi118@gmail.com>
2018-08-09 19:50:28 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
     end
   end
 
+<<<<<<< HEAD
   post '/signup' do
     if params[:user].find{|requirement, value| value == ""}
       redirect '/signup'
@@ -28,6 +29,29 @@ class UsersController < ApplicationController
     end
   end
 
+=======
+  get '/login' do
+    if logged_in?
+      redirect '/tweets'
+    else
+      erb :'/users/login'
+    end
+  end
+
+  post '/signup' do
+    if logged_in?
+      redirect '/tweets'
+    elsif params[:user].find{|requirement, value| value == ""}
+      redirect '/signup'
+    else
+      @user = User.create(params[:user])
+      session[:user_id] = @user.id
+      redirect '/tweets'
+    end
+
+  end
+
+>>>>>>> bf198fa277d9cfa8a0b3e484bc52b67cc86451d8
   post '/login' do
     @user = User.find_by(email: params[:user][:email])
 
